d47170853b3fc70d733b2cbeca106c0520e228a9
Abhimanyu <151956314+Abhimanyu224462@users.noreply.github.com>
2023-12-19 03:07:16 +0530

    varient selection completed

--- src/app/cars/cars-varient/cars-varient.component.ts
+++ src/app/cars/cars-varient/cars-varient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
 import { HttpService } from 'src/app/core/services/http.service';
 
 @Component({
@@ -9,29 +10,50 @@ import { HttpService } from 'src/app/core/services/http.service';
 export class CarsVarientComponent {
   varientsData:any
   varientsFuelData:any
-
+  varientsEFuelData:any
   constructor(private http:HttpService){
 
   }
 ngOnInit(){
   this.getData()
   this.FuelData()
+  this.electricFuelData()
 }
-
+petrolModel:any
 getData(){
   this.http.getDataFromServer("get-variant").subscribe((response:any) =>{
     if(response && response.length>0){
       this.varientsData = response
-      console.log("varients data rec" , this.varientsData)
-    }
+      console.log("varients data rec" , this.varientsData) 
+      // const filteredPeople = people.filter(person => person.name === "John Doe" && person.age > 25);
+      // this.petrolModel = this.varientsData.map((el:any)=>el.modelList["Fuel Type"])
+      // this.petrolModel = this.varientsData.filter((el:any) => el.modelList["Fuel Type"]==="Electric"
+      this.petrolModel = this.varientsData.flatMap((item:any) => item.modelList.filter((variant:any) => variant["Fuel Type"] === "Petrol"));
+      console.log(this.petrolModel);
+      //  this.petrolModel = this.varientsData.filter((el:any) => (el.modelList["Fuel Type"] == "Petrol"))
+      console.log("models with Petrol" , this.petrolModel)
+    }  
   })
 }
 
+dieselModel:any
 FuelData(){
   this.http.getDataFromServer("get-variant").subscribe((response1:any) =>{
     if(response1 && response1.length>0){
       this.varientsFuelData = response1
       console.log("varients Fuel data rec" , this.varientsFuelData)
+      this.dieselModel = this.varientsFuelData.flatMap((item:any) => item.modelList.filter((variant:any) => variant["Fuel Type"] === "Diesel"));
+    }
+  })
+}
+
+electricModel:any
+electricFuelData(){
+  this.http.getDataFromServer("get-variant").subscribe((response2:any) =>{
+    if(response2 && response2.length>0){
+      this.varientsEFuelData = response2
+      console.log("electric varients Fuel data rec" , this.varientsEFuelData)
+      this.electricModel = this.varientsEFuelData.flatMap((item:any) => item.modelList.filter((variant:any) => variant["Fuel Type"] === "Electric"));
     }
   })
 }
